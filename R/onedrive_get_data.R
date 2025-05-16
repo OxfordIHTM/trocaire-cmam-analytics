de339443f0125badf775cbf5205e5bd7608a5be4
Ernest Guevarra <ernest@guevarra.io>
2025-05-16 10:23:11 +0100

    setup data retrieval and processing workflow; fix #6

---
+++ R/onedrive_get_data.R
@@ -0,0 +1,50 @@
+#'
+#' List Trocaire data files from One Drive
+#' 
+#' @param od A `ms_drive` class object created via a call to
+#'   [Microsoft365R::get_business_onedrive()] to login to the shared Microsoft
+#'   One Drive service.
+#' @param path Character value for name of directory to access from the shared
+#'   Microsoft One Drive.
+#' 
+#' @returns A [data.frame()] of files contained within `path` in the shared
+#'   Microsoft One Drive.
+#'
+
+onedrive_list_trocaire_files <- function(od,
+                                         path = "trocaire_somalia_cmam") {
+  data_files <- od$list_files(path)
+
+  data_files
+}
+
+
+#'
+#' Download data from One Drive
+#' 
+
+onedrive_download_data <- function(od,
+                                   file_name, dest_dir, 
+                                   overwrite = FALSE) {
+  file_path <- file.path(dest_dir, basename(file_name))
+
+  if (file.exists(file_path)) {
+    if (overwrite) {
+      od$download_file(
+        src = file_name, 
+        dest = file.path(dest_dir, basename(file_name)), 
+        overwrite = overwrite
+      )
+    } else {
+      file_path
+    }
+  } else {
+    od$download_file(
+      src = file_name, 
+      dest = file.path(dest_dir, basename(file_name)), 
+      overwrite = overwrite
+    )
+  }
+  
+  file_path
+}
--- _targets.R
+++ _targets.R
@@ -1,46 +1,74 @@
-################################################################################
-#
-# General Targets Workflow
-#
-################################################################################
+# General Targets Workflow -----------------------------------------------------
 
-## Load libraries and custom functions -----------------------------------------
+
+## Load libraries and custom functions ----
 suppressPackageStartupMessages(source("packages.R"))
 for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
-## Create targets and list targets objects -------------------------------------
 
-### Data targets
+## Data targets
 data_targets <- tar_plan(
-  
+  tar_target(
+    name = onedrive_trocaire,
+    command = Microsoft365R::get_business_onedrive(),
+    cue = tar_cue("always")
+  ),
+  tar_target(
+    name = trocaire_data_files_list,
+    command = onedrive_list_trocaire_files(
+      od = onedrive_trocaire, path = "trocaire_somalia_cmam"
+    ),
+    cue = tar_cue("always")
+  ),
+  trocaire_data_file_names = file.path(
+    "trocaire_somalia_cmam", trocaire_data_files_list$name
+  ),
+  tar_target(
+    name = trocaire_data_files,
+    command = onedrive_download_data(
+      od = onedrive_trocaire, file_name = trocaire_data_file_names, 
+      dest_dir = "data-raw"
+    ),
+    pattern = trocaire_data_file_names,
+    format = "file"
+  ),
+  tar_target(
+    name = trocaire_cmam_data_raw,
+    command = cmam_read_data(trocaire_data_files),
+    pattern = trocaire_data_files
+  ),
+  tar_target(
+    name = trocaire_cmam_data,
+    command = cmam_process_data(trocaire_cmam_data_raw)
+  )
 )
 
 
-### Processing targets
+## Processing targets
 processing_targets <- tar_plan(
   
 )
 
 
-### Analysis targets
+## Analysis targets
 analysis_targets <- tar_plan(
   
 )
 
 
-### Output targets
+## Output targets
 output_targets <- tar_plan(
   
 )
 
 
-### Reporting targets
+## Reporting targets
 report_targets <- tar_plan(
   
 )
 
 
-### Deploy targets
+## Deploy targets
 deploy_targets <- tar_plan(
   
 )
